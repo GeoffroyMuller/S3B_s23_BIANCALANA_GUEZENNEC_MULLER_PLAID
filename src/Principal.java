93d98a70ec5a4b67e219c9d406442de88d43b33e
lucasguezennec <lucas.guezennec2@etu.univ-lorraine.fr>
2018-11-27 15:14:07 +0100

    Ajout fenetre & barre outils

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,34 @@
+import vue.BarreOutils;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class Principal {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(
+                    "com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        }
+        catch (UnsupportedLookAndFeelException e) {
+            // handle exception
+        }
+        catch (ClassNotFoundException e) {
+            // handle exception
+        }
+        catch (InstantiationException e) {
+            // handle exception
+        }
+        catch (IllegalAccessException e) {
+            // handle exception
+        }
 
+        //Barre d'outil
+        BarreOutils barreOutils = new BarreOutils();
+        JFrame fenetre = new JFrame("EtuPlacement");
+
+        fenetre.setJMenuBar(barreOutils);
+        fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        fenetre.setSize(new Dimension(400,400));
+        fenetre.setVisible(true);
+    }
 }
