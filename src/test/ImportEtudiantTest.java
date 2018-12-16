85a4d7f8e713037d5ad6a44320ae2cdb59f91262
unknown <lucas.guezennec2@etu.univ-lorraine.fr>
2018-12-16 16:54:34 +0100

    Fixe bug

--- src/test/ImportEtudiantTest.java
+++ src/test/ImportEtudiantTest.java
@@ -9,27 +9,6 @@ import org.junit.Test;
 
 public class ImportEtudiantTest {
 
-    /*@Test
-    public void testConstructeur(){
-        Tableau test = new Tableau("C:\\Users\\Lucas\\Desktop\\Classeur1.xlsx","Feuil1");
-
-        //Verification des nom des colonnes
-        assertEquals("La premiere valeur de l'en-tête devrait être test","test",test.getNomDesColonnes()[0]);
-        assertEquals("La deuxième valeur de l'en-tête devrait être test1","test2",test.getNomDesColonnes()[1]);
-        assertEquals("La troisiéme valeur de l'en-tête devrait être grp","grp",test.getNomDesColonnes()[2]);
-
-        //Verification du contenu
-        assertEquals("La premiere valeur de la deuxieme ligne devrait être ligne2Test","ligne2test",test.getTableau()[0][0]);
-        assertEquals("La deuxieme valeur de la deuxieme ligne devrait être ligne2Test2","ligne2test2",test.getTableau()[0][1]);
-        assertEquals("La troisieme valeur de la deuxieme ligne devrait être a","a",test.getTableau()[0][2]);
-
-        //Verification des groupes detecte
-        assertEquals("Le nombre de groupe detecte devrait être 2",2,test.getGroupeTrouveDansLeDernierFichier().size());
-        assertTrue(test.getGroupeTrouveDansLeDernierFichier().contains(new Groupe("A")));
-        assertTrue(test.getGroupeTrouveDansLeDernierFichier().contains(new Groupe("B")));
-
-    }*/
-
     @Test
     public void testConstructeur(){
         ImportEtudiant test = new ImportEtudiant("C:\\Users\\Lucas\\Desktop\\Classeur1.xlsx","Feuil1");
