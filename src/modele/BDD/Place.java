8a1e0c1d78a29082e053a77d7deb68b1c958930d
joyscraft <justin.plaid8@etu.univ-lorraine.fr>
2019-01-04 13:18:15 +0100

    place avec i et j

--- src/modele/BDD/Place.java
+++ src/modele/BDD/Place.java
@@ -14,13 +14,17 @@ public class Place {
 	private String typeSalle;
 	private int idSalle;
 	private int idPlace;
+	private int j;
+	private int i;
 
 
-	public Place(String nom, String typeSalle, int idSalle) {
+	public Place(String nom, String typeSalle, int i, int j, int idSalle) {
 		this.idPlace=-1;
 		this.nom=nom;
 		this.typeSalle=typeSalle;
 		this.idSalle=idSalle;
+		this.j=j;
+		this.i=i;
 	}
 
 	
@@ -35,10 +39,28 @@ public class Place {
 
 
 	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+
+	/**
 	 * @return the typeSalle
 	 */
 	public String getTypeSalle() {
 		return typeSalle;
+	}
+
+
+
+	/**
+	 * @param typeSalle the typeSalle to set
+	 */
+	public void setTypeSalle(String typeSalle) {
+		this.typeSalle = typeSalle;
 	}
 
 
@@ -53,6 +75,15 @@ public class Place {
 
 
 	/**
+	 * @param idSalle the idSalle to set
+	 */
+	public void setIdSalle(int idSalle) {
+		this.idSalle = idSalle;
+	}
+
+
+
+	/**
 	 * @return the idPlace
 	 */
 	public int getIdPlace() {
@@ -61,9 +92,56 @@ public class Place {
 
 
 
-	private Place(String nom, String typeSalle, int idSalle, int idPlace) {
+	/**
+	 * @param idPlace the idPlace to set
+	 */
+	public void setIdPlace(int idPlace) {
+		this.idPlace = idPlace;
+	}
+
+
+
+	/**
+	 * @return the j
+	 */
+	public int getJ() {
+		return j;
+	}
+
+
+
+	/**
+	 * @param j the j to set
+	 */
+	public void setJ(int j) {
+		this.j = j;
+	}
+
+
+
+	/**
+	 * @return the i
+	 */
+	public int getI() {
+		return i;
+	}
+
+
+
+	/**
+	 * @param i the i to set
+	 */
+	public void setI(int i) {
+		this.i = i;
+	}
+
+
+
+	private Place(String nom, String typeSalle, int idSalle,int i, int j, int idPlace) {
 		this.typeSalle=typeSalle;
 		this.nom=nom;
+		this.j=j;
+		this.i=i;
 		this.idPlace=idPlace;
 		this.idSalle=idSalle;
 	}
@@ -74,7 +152,9 @@ public class Place {
 			Connection connect=DBConnection.getConnection();
 			String SQLPrep0 = "CREATE TABLE IF NOT EXISTS `etuplacement`.`Place` "
 					+ "( `idPlace` INT(11) NOT NULL AUTO_INCREMENT , `nom` VARCHAR(40) NOT NULL,"
-					+ " `typePlace` VARCHAR(40) NOT NULL, `idSalle` INT(11) NOT NULL, "
+					+ " `typePlace` VARCHAR(40) NOT NULL,`i` INT(11) NOT NULL,"
+					+ "`j` INT(11) NOT NULL,"
+					+ " `idSalle` INT(11) NOT NULL, "
 					+ "PRIMARY KEY (`idPlace`), "
 					+ "FOREIGN KEY (idSalle) REFERENCES Salle (idSalle)) ENGINE = InnoDB";
 			PreparedStatement prep0 = connect.prepareStatement(SQLPrep0);
@@ -113,8 +193,10 @@ public class Place {
 		while (rs.next()) {
 			String resNom = rs.getString("nom");
 			String resTypeSalle = rs.getString("typeSalle");
+			int resI= rs.getInt("i");
+			int resJ = rs.getInt("j");
 			int resIdSalle = rs.getInt("idSalle");
-			res = new Place(resNom, resTypeSalle, resIdSalle, id);
+			res = new Place(resNom, resTypeSalle, resIdSalle,resI, resJ, id);
 		}
 		return res;
 	}
@@ -132,7 +214,9 @@ public class Place {
 			String resNom = rs.getString("nom");
 			String resTypeSalle = rs.getString("typeSalle");
 			int resIdSalle = rs.getInt("idSalle");
-			res.add(new Place(resNom, resTypeSalle, resIdSalle, id));
+			int resI= rs.getInt("i");
+			int resJ = rs.getInt("j");
+			res.add(new Place(resNom, resTypeSalle, resIdSalle, resI, resJ, id));
 		}
 		return res;
 	}
@@ -153,6 +237,21 @@ public class Place {
 		}
 	}
 
+	/**
+	 * DeleteSalle.
+	 */
+	public void deleteSalle(int id){
+		try {
+			Connection connect=DBConnection.getConnection();
+			String SQLPrep0 = "DELETE FROM Place WHERE idSalle='"+id+"';";
+			PreparedStatement prep0 = connect.prepareStatement(SQLPrep0);
+			prep0.execute();
+		}
+		catch(SQLException e) {
+			System.out.println(e.getMessage()+" Delete salle"+e.getErrorCode()+e.toString());
+		}
+	}
+	
 	/**
 	 * Save.
 	 */
